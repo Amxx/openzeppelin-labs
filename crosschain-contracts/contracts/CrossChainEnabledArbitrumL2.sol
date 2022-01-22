a528e55ba6d64edfdb173d4b8a915fdde160c308
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-01-22 15:01:00 +0100

    refactor CrossChainEnabledArbitrumL2 to use ArbSys

--- crosschain-contracts/contracts/CrossChainEnabledArbitrumL2.sol
+++ crosschain-contracts/contracts/CrossChainEnabledArbitrumL2.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./interfaces/IArbSys.sol";
 import "./CrossChainEnabled.sol";
 
 abstract contract CrossChainEnabledArbitrumL2 is CrossChainEnabled {
-    uint160 constant offset = uint160(0x1111000000000000000000000000000000001111);
+    IArbSys internal constant arbsys = IArbSys(0x0000000000000000000000000000000000000064);
 
     function _isCrossChain() internal view virtual override returns (bool) {
-        return true; // TODO: can we detect that ?
+        return arbsys.isTopLevelCall();
     }
 
     function _crossChainSender() internal view virtual override onlyCrossChain() returns (address) {
-        unchecked {
-            return address(uint160(msg.sender) - offset); // TODO: if crosschain, then get L1 by reverting L1toL2Alias
-        }
+        return arbsys.wasMyCallersAddressAliased()
+            ? arbsys.myCallersAddressWithoutAliasing()
+            : msg.sender;
     }
 }
