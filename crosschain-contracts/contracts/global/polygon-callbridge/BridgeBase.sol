c2be58a527c9a8810dda52b635f714162741571d
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-02-14 18:01:46 +0100

    add a retry mechanism for the polygon bridge

--- crosschain-contracts/contracts/global/polygon-callbridge/BridgeBase.sol
+++ crosschain-contracts/contracts/global/polygon-callbridge/BridgeBase.sol
@@ -2,14 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "./IBridge.sol";
 
 // see: "@eth-optimism/contracts/libraries/constants/Lib_DefaultValues.sol";
 address constant DEFAULT_SENDER = 0x000000000000000000000000000000000000dEaD;
 
 abstract contract BrigdeBase is IBridge {
-    event CallSuccess();
-    event CallFailure(bytes returndata);
+    using Counters for Counters.Counter;
+
+    event CallSuccess(address indexed sender, address indexed target, bytes data);
+    event CallFailure(address indexed sender, address indexed target, bytes data);
+    event RetrySuccess(address indexed sender, address indexed target, bytes data);
+
+    mapping(bytes32 => Counters.Counter) private retryTickets;
 
     address internal _sender = DEFAULT_SENDER;
 
@@ -18,17 +24,31 @@ abstract contract BrigdeBase is IBridge {
         return _sender;
     }
 
+    function retry(address sender, address target, bytes calldata data) external {
+        retryTickets[keccak256(abi.encode(sender, target, data))].decrement(); // revert if overflow
+
+        (bool success, bytes memory returndata) = _tryExecute(sender, target, data);
+        Address.verifyCallResult(success, returndata, "Call execution failure");
+
+        emit RetrySuccess(sender, target, data);
+    }
+
     function _forward(address sender, address target, bytes memory data) internal {
-        address previousSender = _sender;
-        _sender = sender;
-        (bool success, bytes memory returndata) = target.call(data);
-        _sender = previousSender;
+        (bool success,) = _tryExecute(sender, target, data);
 
         if (success) {
-            emit CallSuccess();
+            emit CallSuccess(sender, target, data);
         } else {
-            emit CallFailure(returndata);
+            retryTickets[keccak256(abi.encode(sender, target, data))].increment();
+            emit CallFailure(sender, target, data);
         }
+    }
+
+    function _tryExecute(address sender, address target, bytes memory data) internal returns (bool success, bytes memory returndata) {
+        address previousSender = _sender;
+        _sender = sender;
+        (success, returndata) = target.call(data);
+        _sender = previousSender;
     }
 
     function _encodeMessage(address sender, address target, bytes memory data) internal pure returns (bytes memory message) {
